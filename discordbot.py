f579d94456b28b84dd5a8f02a04fabb3c011c577
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-11-16 21:34:34 +0900

    discordbot.oy

--- discordbot.py
+++ discordbot.py
@@ -1,1 +1,30 @@
+import discord
+from discord.ext import commands
+import os
 
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+bot = commands.Bot(
+    command_prefix=["Cu!", "cu!"],
+    help_command=None,
+    intents=intents,
+    allowed_mentions=discord.AllowedMentions(replied_user=False, everyone=False),
+)
+token = os.environ["token"]
+
+def restart_bot():
+  os.execv(sys.executable, ['python'] + sys.argv)
+  
+@bot.event
+async def on_ready():
+    servers = len(bot.guilds)
+    members = 0
+    for guild in bot.guilds:
+        members += guild.member_count - 1
+    await bot.change_presence(
+        activity=discord.Activity(name=f"Cu!help | {str(servers)}servers | {str(members)}users", type=3)
+    )
+
+bot.load_extension("sub")
+
+bot.run(token)
