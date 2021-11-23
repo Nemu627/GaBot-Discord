ef720df9e6699cc4fab9c376965d576fcfcffc93
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-11-23 21:17:28 +0900

    なんで

--- discordbot.py
+++ discordbot.py
@@ -18,16 +18,6 @@ token = os.environ["token"]
 def restart_bot():
   os.execv(sys.executable, ['python'] + sys.argv)
 
-@bot.event
-async def on_ready():
-    servers = len(bot.guilds)
-    members = 0
-    for guild in bot.guilds:
-        members += guild.member_count - 1
-    await bot.change_presence(
-        activity=discord.Activity(name=f"Cu!help | {str(servers)}servers | {str(members)}users", type=3)
-    )
-
 bot.load_extension("Cogs.event")
 bot.load_extension("Cogs.bot")
 bot.load_extension("Cogs.variety")
