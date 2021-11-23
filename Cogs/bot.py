6f7f0dbf0a864dc46816116df372b879bd1f1dca
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-11-23 21:09:25 +0900

    renew bot.py

--- Cogs/bot.py
+++ Cogs/bot.py
@@ -1,1 +1,24 @@
+import discord
+from discord.ext import commands
+import asyncio
+import random
 
+class AppCmdBot(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self._last_member = None
+        
+    @commands.command()
+    async def help(self,ctx):
+        await ctx.reply("coming soon ...")
+
+    @commands.command()
+    async def ping(self,ctx):
+        async with ctx.typing():
+            await asyncio.sleep(0)
+        embed = discord.Embed(title="PING", description=f"ただいまのping値は**{round(self.bot.latency * 1000)}**msです！",
+                              color=0x3498DB)
+        await ctx.reply(embed=embed, mention_author=False)
+
+def setup(bot):
+    return bot.add_cog(AppCmdBot(bot))
