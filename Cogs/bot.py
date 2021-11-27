41ee8519bcb75238be502dcba9f2433dc6b6300a
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-11-27 20:07:37 +0900

    list

--- Cogs/bot.py
+++ Cogs/bot.py
@@ -11,6 +11,26 @@ class AppCmdBot(commands.Cog):
     @commands.command()
     async def help(self,ctx):
         await ctx.reply("coming soon ...")
+        
+    @commands.command()
+    async def list(self,ctx):
+        embed = discord.Embed(title="コマンドリスト", description="使用可能なコマンド一覧です♪\n各コマンドの詳細は`Cu!help [コマンド名]`で確認できます♪", colour=0x3498DB)
+        embed1add_field(
+            name=":robot: 》BOT",
+            value="`help`：困ったときはを表示します。\n`list`：コマンドリストを表示します。\n`prof`：CuBOTのプロフィールを表示します。\n`ping`：CuBOTのping値を表示します。",
+        )
+        embed.add_field(
+            name=":video_game: 》バラエティ",
+            value="`fortune`：おみくじが引けます。\n"
+                  "`rps`：じゃんけんができます。\n"
+                  "`dice`：サイコロを振れます。\n"
+                  "`pun`：ダジャレが聞けます。\n"
+                  "`cquiz`：暗算クイズができます。\n"
+                  "`coin`：コイントスができます。\n"
+                  "`slot`：スロットができます。\n"
+                  "`totusi`：突然の死AAを作成します。",
+        )
+        await ctx.reply(embed=embed)
 
     @commands.command()
     async def ping(self,ctx):
