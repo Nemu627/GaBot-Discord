724ff0a321609044b3a903f5d2431fab05ec25b8
音夢?! <82650472+Nemu627@users.noreply.github.com>
2021-12-20 18:47:59 +0900

    fix : プレフィックスの修正

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,7 @@ import os
 intents = discord.Intents.all()
 client = discord.Client(intents=intents)
 bot = commands.Bot(
-    command_prefix=["GA!", "Ga!", "gA", "ga"],
+    command_prefix=["GA!", "Ga!", "gA!", "ga!"],
     help_command=None,
     intents=intents,
     allowed_mentions=discord.AllowedMentions(replied_user=False, everyone=False),
